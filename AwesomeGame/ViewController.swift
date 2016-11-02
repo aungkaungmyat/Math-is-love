fad1fe4825b39556f65273ef1815a25b54b942df
Aung Myat <amyat@purdue.edu>
2016-11-02 19:59:24 -0400

    Initial Commit

---
+++ AwesomeGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AwesomeGame
+//
+//  Created by Mr.Awesome on 11/2/2559 BE.
+//  Copyright © 2559 BE me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
