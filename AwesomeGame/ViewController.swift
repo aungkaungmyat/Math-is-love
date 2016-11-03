bdd72d39a5454f912905b41f92b5810cd94fbacc
Aung Myat <amyat@purdue.edu>
2016-11-02 20:21:05 -0400

    Testing

--- AwesomeGame/ViewController.swift
+++ AwesomeGame/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    //commenting here
+    //just testing
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
