e4fee74f9104a1ca33931799ce4a55809ab7578c
Aung Myat <amyat@purdue.edu>
2016-11-25 21:17:29 -0500

    Done alot

--- AwesomeGame/ViewController.swift
+++ AwesomeGame/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AddedScore: UILabel!
+    @IBOutlet weak var RequiredScore: UILabel!
+    @IBOutlet weak var One_One: UIButton!
+    @IBOutlet weak var One_Two: UIButton!
+    var a = 10
+    var b = 15
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +31,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func startTheGame(_ sender: AnyObject) {
+        
+        One_One.setTitle(String(10), for: .normal)
+        One_One.setTitleColor( UIColor.black , for: .normal)
+        One_Two.setTitle(String(15), for: .normal)
+        One_Two.setTitleColor(UIColor.red , for: .normal)
+    }
+    
+    @IBAction func dataFromOne_One(_ sender: AnyObject) {
+        print("One_One is working")
+    }
+    
+    @IBAction func dataFromOne_Two(_ sender: AnyObject) {
+        print("One_Two is working")
+    }
+    
+    
+    
+    
 
 }
 
